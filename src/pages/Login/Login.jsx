bbaeb49e29f3b041b45f7bad02add81da75c6352
DaniM0613 <wwh.rj.92@gmail.com>
2024-06-18 23:43:10 -0600

    creando el log in de Netflix y agregando css

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,38 @@
 import './Login.css'
+import logo from '../../assets/logo.png'
+import { useState } from 'react'
 
  const Login = () => {
+
+   const [signState, setSignState] = useState("Sign In")
+
+
   return (
-    <div>
-      
+    <div className='login'>
+      <img src={logo} alt='' className='login-logo'/>
+       <div className='login-form'>
+        <h1>{signState}</h1>
+        <form>
+          {signState==="Sign Up" ? <input type='text' placeholder='Your Name'/> : <></>}
+          
+          <input type='email' placeholder='Your Email'/>
+          <input type='password' placeholder='Your Password'/>
+          <button>{signState}</button>
+          <div className='form-help'>
+           <div className='remember'>
+           <input type='checkbox'/>
+            <label htmlFor=''>Remember Me</label>
+          </div>
+          <p>Need Help?</p>
+           </div>
+        </form>
+        <div className='form-switch'>
+          {signState==="Sign In" 
+          ? <p>New To Netflix? <span onClick={()=>{setSignState("Sign Up")}}>Sign Up Now</span></p> 
+          : <p>Already have account?<span onClick={()=>{setSignState("Sign In")}}>Sign in Now</span></p>
+          }   
+        </div>
+       </div>
     </div>
   )
 }
